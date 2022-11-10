dbae10bb49e5dec5d123219b499885babcb38eb2
Fang <me@fangqk.com>
2022-11-10 19:52:07 +0800

    (config): production 支持环境变量传递

--- config/production.js
+++ config/production.js
@@ -1,1 +1,26 @@
-module.exports = {}
+const envData = process.env || {}
+
+module.exports = {
+  FangchaAuth: {
+    configVersion: '0.0.0',
+    wecomBotKey: envData.wecomBotKey,
+    adminBaseURL: envData.adminBaseURL,
+    adminPort: 2600,
+    adminJwtKey: 'sso_admin_jwt_key',
+    adminJwtSecret: envData.adminJwtSecret,
+    mysql: {
+      ssoDB: {
+        host: envData.DB_Host,
+        port: envData.DB_Port,
+        dialect: 'mysql',
+        database: envData.DB_Name,
+        username: envData.DB_User,
+        password: envData.DB_Password,
+      },
+    },
+    WebAuth: {
+      usernameRetained: envData.Auth_User,
+      passwordRetained: envData.Auth_Password,
+    },
+  },
+}
